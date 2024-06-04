cbe49dfa2b319dde9cfff7a66d8b59925b347e4a
mwangiryan12 <mwangiryan12@gmail.com>
2024-06-04 20:49:04 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    def match(self,lst):
+        self.lst = lst
+        arr = []
+        for wor in lst:
+            lettersArr = [letter for letter in wor]
+            if sorted(lettersArr) == sorted(list(self.word)):
+              
+                print(wor)
+                arr.append(wor)
+        return arr
+
+word = Anagram("enlist")
+print(word.match(["listen", "poison", "hello"]))
